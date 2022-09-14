de96e541124339fb922d74882b342f0599e24ced
0xelia <elia.vanon@yahoo.com>
2022-09-14 12:32:09 +0200

    vars and main components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderComp/>
+    <MainComp/>
+    <FooterComp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComp from './components/headerComp.vue';
+import MainComp from './components/mainComp.vue';
+import FooterComp from './components/footerComp.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HeaderComp,
+    MainComp,
+    FooterComp
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  #app {
+    @import './style/generals.scss';
+    
 }
 </style>
---
+++ src/components/headerComp.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>
+        
+    </header>
+</template>
+
+<script>
+    export default {
+        name: 'headerComp'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
