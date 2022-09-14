61ef58d99f448dbd9f49724175473c6d8a924e9d
0xelia <elia.vanon@yahoo.com>
2022-09-14 11:32:07 +0200

    bootstrap and icons setop

--- src/main.js
+++ src/main.js
@@ -3,6 +3,29 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+
+//BOOTSTRAP
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+
+
+//FONTAWSOME
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+
+
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
